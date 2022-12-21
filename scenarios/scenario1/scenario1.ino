a2f5c31d8d8918f20355247cfc1326906fb2ed55
DaMalle <DaMalle>
2022-12-21 11:26:34 +0100

    added stuff

--- scenarios/scenario1/scenario1.ino
+++ scenarios/scenario1/scenario1.ino
@@ -2,56 +2,87 @@
 #include<Zumo32U4.h>
 #include<BasicLinearAlgebra.h>
 #include<math.h>
+#include <VL53L0X.h>
+
+VL53L0X tof;
 
 Zumo32U4IMU imu;
 Zumo32U4Motors motors;
+Zumo32U4Buzzer buzzer;
 
 #include"TurnSensor.h"
 
-const int trigPinF = 1, trigPinB = 30, trigPinL = 17, trigPinR = 0,
-          echoPinF = 4, echoPinB = 20, echoPinL = 18, echoPinR = 21;
+const int trigPinF = 1, trigPinB = 30, trigPinL = 17,
+          echoPinF = 4, echoPinB = 20, echoPinL = 18;
+
+//array for noice reduction. 
+int arraySize = 10;
+double array[10];
+//variables
+double error;
+double lastError = 0;
+double currentTime = millis();
+double previousTime = 0;
+double elapsedTime;
+
+double cumError=0, rateError;
+int motorspeed;
+int currentAngle;
+int regulatedSpeedL = 208; // this it the left motorspeed with closest equality to the right 200 motor speed.
+int baseSpeedL = 208;
+int baseSpeedR = 200;
+int regulatedSpeedR = 200;
+int status = 'f'; // 'f' for fine or follow wall 'c' for corner 'o' for obstacle
+
+int Angle;
+double getDistance(int trig, int echo) {
+  Angle= getAngle();
+  digitalWrite(trig, LOW); // clear
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH); // Trigger pulse
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW); // stop pulse
+
+  // Return distance
+  return pulseIn(echo, HIGH) * 0.034029 /2; // Speed of sound wave divided by 2 (go and back)
+}
 
 void setup(void) {
-    Serial.begin(9600);
+    //Serial.begin(9600);
+    Wire.begin();
 
-    pinMode(trigPinR, OUTPUT);
     pinMode(trigPinB, OUTPUT);
     pinMode(trigPinF, OUTPUT);
     pinMode(trigPinL, OUTPUT);
 
-    pinMode(echoPinR, INPUT);
     pinMode(echoPinB, INPUT);
     pinMode(echoPinF, INPUT);
     pinMode(echoPinL, INPUT);
+
+    tof.init();
+    tof.setTimeout(500);
+    tof.setMeasurementTimingBudget(100000);
 
     delay(1000);
     turnSensorSetup();
     turnSensorReset();
 
-    calibrateWall(trigPinF, echoPinF);
-    delay(1000);
-    calibrateWall(trigPinF, echoPinF);
-    Serial.println("current angle: " + (String)getGyroAngle()); 
+    calibrateWall(); 
 }
 
-void loop(void) {
-    //Serial.println(getGyroAngle());
-    //delay(1000);
-}
-
-void calibrateWall(const int trig, const int echo) {
-    const int dataPointsTotal = 5;
+void calibrateWall() {
+    const int dataPointsTotal = 7;
     int angle;
     double length;
-    int arr[dataPointsTotal] = {0, 10, 13, -10, -13};// {0, 5, 10, -5, -10};
+    int arr[dataPointsTotal] = {0, 5, 10, 15, -5, -10, -15};
 
     BLA::Matrix<dataPointsTotal, 2> X;
     BLA::Matrix<dataPointsTotal, 1> b;
       
     for(int i=0; i<dataPointsTotal; i++) {
         rotateToTarget(arr[i]);
-        angle = getGyroAngle();
-        length = getUltraSonicSensorData(trig, echo);
+        angle = getAngle();
+        length = tof.readRangeSingleMillimeters() * 0.1;
     
         Serial.println("angle: " + (String) angle + " Lenght: " + (String) length);
 
@@ -61,7 +92,7 @@ void calibrateWall(const int trig, const int echo) {
 
         delay(50);
     }
-    
+
     BLA::Matrix<2, dataPointsTotal> X_transpose = ~X;
     BLA::Matrix<2, 2> inversed_X_transpose_dot_X = X_transpose * X;
     BLA::Invert(inversed_X_transpose_dot_X);
@@ -76,7 +107,7 @@ void calibrateWall(const int trig, const int echo) {
     rotateToTarget(round(atan2(y, x) * 180 / M_PI));
 }
 
-int32_t getGyroAngle(void) {
+int32_t getAngle(void) {
     turnSensorUpdate();
     return (((int32_t)turnAngle >> 16) * 360) >> 16;
 }
@@ -94,13 +125,84 @@ double getUltraSonicSensorData(const int trig, const int echo) {
 
 void rotateToTarget(const int target) {  // span {-180 -- 0 -- 180}
     const int turnSpeed = 75;
-    int currentAngle = getGyroAngle();
+    int currentAngle = getAngle();
     Serial.println("current angle: " + (String) currentAngle);
 
     while(target != currentAngle) {
-        currentAngle = getGyroAngle();
+        currentAngle = getAngle();
         if(target < currentAngle) motors.setSpeeds(turnSpeed, -turnSpeed);
         else if(target > (currentAngle)) motors.setSpeeds(-turnSpeed, turnSpeed);
     }
     motors.setSpeeds(0,0);
+}
+
+double computePID(int setPoint, double input, float kp, float ki, float kd){ 
+  currentTime = millis();  
+  elapsedTime = currentTime - previousTime;
+  //Serial.println("elapsedTime: " + (String) elapsedTime);
+  error = setPoint - input;      // determine error
+  cumError += error * elapsedTime;   // compute integral
+  rateError = (error - lastError) / elapsedTime;   // compute derivative
+  double out = kp*error + ki*cumError + kd*rateError;                //PID output               
+  //Serial.println("error: " + (String) error);
+  //Serial.println("cumError: " + (String) cumError);
+  //Serial.println("rateError: " + (String) rateError);
+  //lastError = error;                                //remember current error
+  previousTime = currentTime;
+  return out;                                        //have function return the PID output
+}
+
+float localError = 0;
+int output;
+char followWall(){
+  while(getDistance(trigPinF,echoPinF)>15) {
+    double inputVar = cos(getAngle() * M_PI / 180) * (tof.readRangeSingleMillimeters() * 0.1);
+    if(inputVar >= 50) {
+      motors.setSpeeds(0, 0); 
+      return 'c'; // 'c' for corner
+    }
+    else {
+      int change = computePID(30, inputVar, 0.5 , 0, 0); // P1.5 , I0.00004 , D0.000001
+      if (change > 200) change = 200;
+      //Serial.println("right: " + (String) right);
+      //Serial.println("change: " + (String) change);    
+      motors.setSpeeds(baseSpeedL-change, baseSpeedR + change); 
+    }
+  }
+  motors.setSpeeds(0, 0);
+  return 'o'; // 'o' for obstacle
+}
+
+void corner(){
+  if (getDistance(trigPinF,echoPinF)<20) motors.setSpeeds(0,0);
+  else{
+    buzzer.playFrequency(1200,1000,15);
+    turnSensorReset();
+    motors.setSpeeds(250, 150);
+    while(getAngle() != -90+1);
+    motors.setSpeeds(0,0);
+    turnSensorReset();
+    motors.setSpeeds(208,200);
+    delay(1000);
+    calibrateWall();
+    turnSensorReset();
+    cumError=0;
+
+    
+    status = 'f';
+  }
+}
+
+void loop() {
+  followWall();
+  switch (status){
+    case 'f': status = followWall(); break;
+    case 'c': corner(); break;
+    case 'o':
+    buzzer.playFrequency(1200,1000,15);
+    motors.setSpeeds(0, 0);
+    status = 'A';
+    break;
+  }
+  //motors.setSpeeds(208, 200);
 }
--- test/test.ino
+++ test/test.ino
@@ -1,12 +1,18 @@
 #include<Wire.h>
-#include<Zumo32U4.h>
+//#include<Zumo32U4.h>
+#include <VL53L0X.h>
+
+VL53L0X tof;
 
 const int trigPinF = 1, trigPinB = 30, trigPinL = 17, trigPinR = 0,
           echoPinF = 4, echoPinB = 20, echoPinL = 18, echoPinR = 21;
  
 void setup() {
   Serial.begin(9600);
-  pinMode(trigPinR, OUTPUT);
+  Wire.begin();
+  I2Cscan();
+
+  /*pinMode(trigPinR, OUTPUT);
   pinMode(trigPinB, OUTPUT);
   pinMode(trigPinF, OUTPUT);
   pinMode(trigPinL, OUTPUT);
@@ -14,11 +20,61 @@ void setup() {
   pinMode(echoPinR, INPUT);
   pinMode(echoPinB, INPUT);
   pinMode(echoPinF, INPUT);
-  pinMode(echoPinL, INPUT);
+  pinMode(echoPinL, INPUT);*/
+  
+  tof.init();
+  tof.setTimeout(500);
+  tof.setMeasurementTimingBudget(100000);
+
+  //tof.startContinuous();
 }    
  
 void loop() {
-  Serial.println(getDistance(trigPinF, echoPinF));
+  Serial.println(tof.readRangeSingleMillimeters());
+  //I2Cscan();
+  //delay(1000);
+}
+
+void I2Cscan()
+{
+// scan for i2c devices
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+    
 }
 
 double getDistance(int trig, int echo) {
